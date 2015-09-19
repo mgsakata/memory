64ab6e8ae03125d303cd67522fbb9a086a9447da
Mike Sakata <mgsakata@gmail.com>
2015-09-19 16:29:21 -0300

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Memory App!"
       redirect_to @user
     else
